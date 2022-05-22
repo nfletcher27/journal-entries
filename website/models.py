582ec6ff90931015fc8cb345e1df12cb56984abb
Nesta <89424122+nfletcher27@users.noreply.github.com>
2022-05-22 01:15:55 -0400

    Update models.py

--- website/models.py
+++ website/models.py
@@ -2,14 +2,16 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+# Create a node which takes in a model from our database
 class Note(db.Model):
     
+    # Our notes have an ID which is our column value, data which is a string of a maximum length, the date of creation, and the user ID
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     
-
+# Create a user which takes in our model and a UserMixin which provides easy implementation of authentication and user ID properties
 class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
